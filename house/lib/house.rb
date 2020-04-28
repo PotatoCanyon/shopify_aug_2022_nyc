bdba12bd98c58f222d6084d438787de706f213bd
Sandi Metz <sandi@sandimetz.com>
2020-04-28 16:03:21 -0400

    add MixedColumnOrderer

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -69,6 +69,12 @@ class RandomButLastOrderer
   end
 end
 
+class MixedColumnOrderer
+  def order(data)
+    data.transpose.map {|column| column.shuffle}.transpose
+  end
+end
+
 
 class PiratePrefixer
   def prefix
