a032325198cc651391e0da9b2ad27aa94fd91872
Sandi Metz <sandi@sandimetz.com>
2020-04-28 08:48:40 -0400

    separate forwarding from creation
    This allows you to get a new instance indepenent of the lyrics

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -20,6 +20,14 @@ end
 
 
 class BottleVerse
+  def self.for(number)
+    new(BottleNumber.for(number))
+  end
+
+  def self.lyrics(number)
+    self.for(number).lyrics
+  end
+
   def self.lyrics(number)
     new(BottleNumber.for(number)).lyrics
   end
