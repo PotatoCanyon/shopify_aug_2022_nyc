9b6c0d23767294a4889b630be35a95d6953504a5
Sandi Metz <sandi@sandimetz.com>
2022-08-31 07:17:10 -0400

    polymorphic bottles

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -8,16 +8,18 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number = bottle_number_for(number)
-    next_bottle_number = bottle_number_for(bottle_number.successor)
+    bottle_number = BottleNumber.for(number)
 
-    "#{bottle_number.quantity.capitalize} #{bottle_number.container} of beer on the wall, " +
-    "#{bottle_number.quantity} #{bottle_number.container} of beer.\n" +
+    "#{bottle_number} of beer on the wall, ".capitalize +
+    "#{bottle_number} of beer.\n" +
     "#{bottle_number.action}, " +
-    "#{next_bottle_number.quantity} #{next_bottle_number.container} of beer on the wall.\n"
+    "#{bottle_number.successor} of beer on the wall.\n"
   end
+end
 
-  def bottle_number_for(number)
+
+class BottleNumber
+  def self.for(number)
     case number
     when 0
       BottleNumber0
@@ -27,13 +29,14 @@ class Bottles
       BottleNumber
     end.new(number)
   end
-end
 
-
-class BottleNumber
   attr_reader :number
   def initialize(number)
     @number = number
+  end
+
+  def to_s
+    "#{quantity} #{container}"
   end
 
   def quantity
@@ -41,7 +44,7 @@ class BottleNumber
   end
 
   def container
-     "bottles"
+    "bottles"
   end
 
   def action
@@ -53,7 +56,7 @@ class BottleNumber
   end
 
   def successor
-    number - 1
+    BottleNumber.for(number - 1)
   end
 end
 
@@ -67,7 +70,7 @@ class BottleNumber0 < BottleNumber
   end
 
   def successor
-    99
+    BottleNumber.for(99)
   end
 end
 
